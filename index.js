5d8f5249ead660e41e438e43b6df5c86ce77cb64
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-06-26 09:10:48 +0300

    commit tree 3edbd96f668fca2c2f9c517f3940a0ffb572284a

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email){
+    const objectConfiguration = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({name, email})
+    };
+return fetch("http://localhost:3000/users", objectConfiguration)
+.then((response)=>{
+    return response.json();
+})
+.then((object)=>{
+    document.body.innerHTML = object["id"]
+})
+.catch((error)=>{
+    document.body.innerHTML= error.message
+})
+};
+submitData();
+
